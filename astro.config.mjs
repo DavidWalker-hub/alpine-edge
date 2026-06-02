7ad1cade6cbc4bca672bb5dbfd66c3d96d96fbd2
David Walker <david_walker1@hotmail.co.uk>
2026-06-02 16:09:52 +0100

    remove unused import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig, envField } from "astro/config";
+import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import tailwindcss from "@tailwindcss/vite";
